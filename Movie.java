fbeda7caca30a58098f315c4280f70086c6f19dc
AquaBlaze0010 <50202094+sealnot123@users.noreply.github.com>
2021-03-02 23:54:16 +0700

    Update Movie.java

--- Movie.java
+++ Movie.java
@@ -1,6 +1,20 @@
+// --== CS400 File Header Information ==--
+// Name: Nott Laoaron
+// Email: laoaron@wisc.edu
+// Team: red
+// Role: Data Wrangler
+// TA: Xi Chen
+// Lecturer: Florian Heimerl
+// Notes to Grader: This is object for storing a movie's data
+
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * This class is the class that implements MovieInterface
+ * It represents objects that contains information of movies
+ * @author Nott Laoaron (Data Wrangler)
+ */
 public class Movie implements MovieInterface {
     // 0 = title
     // 1 = original_title
@@ -16,25 +30,55 @@ public class Movie implements MovieInterface {
     // 11 = description
     // 12 = avg_vote
     private ArrayList<String>[] data;
+	
+    /**
+     * The constructor of this class
+     * @param data the list of data of a movie
+     */
     Movie(ArrayList<String>[] data){
         this.data = data;
     }
+	
+    /**
+     * The constructor of this class
+     * @param data the list of data of a movie
+     */
     @Override
 	public String getTitle(){
         return data[0].get(0);
     }
+	
+    /**
+     * Get the year of movie
+     * @return a 4-digit integer of year of movie
+     */
     @Override
 	public Integer getYear(){
         return Integer.parseInt(data[2].get(0));
     }
+	
+    /**
+     * Get the list of genres of this movie
+     * @return the list of strings of genres of this movie
+     */
     @Override
 	public List<String> getGenres(){
         return data[3];
     }
+	
+    /**
+     * Get the name of the director
+     * @return the string of the name of the director
+     */
     @Override
 	public String getDirector(){
         return data[7].get(0);
     }
+	
+    /**
+     * Get the description
+     * @return the string of description of the movie
+     */
     @Override
 	public String getDescription(){
         String fullDescription = "";
@@ -43,6 +87,11 @@ public class Movie implements MovieInterface {
         }
         return fullDescription;
     }
+	
+    /**
+     * Get the average vote
+     * @return the real number between 0.0 and 10.0 (inclusive) representing the score of this movie
+     */
     @Override
 	public Float getAvgVote(){
         return Float.parseFloat(data[12].get(0));
